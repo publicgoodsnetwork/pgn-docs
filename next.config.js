27dec1eb220306a18f27993e27e5d82b08ed959f
lebraat <dan.brunsdon@gmail.com>
2023-09-05 16:01:38 -0600

    Updated meta file, file name, 2 redirects, and added a new redirect

--- next.config.js
+++ next.config.js
@@ -1,47 +1,52 @@
 const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
-
-module.exports = withNextra(
-  {
-    i18n: {
-        locales: ['en'],
-        defaultLocale: 'en'
-      },    
-    async redirects() {
-        return [
-            {
-                source: '/getting-started',
-                destination: '/',
-                permanent: true
-            },
-            {
-                source: '/using-pgn',
-                destination: '/using-pgn/adding-pgn-to-a-wallet',
-                permanent: true
-            },
-            {
-                source: '/getting-started/rpc-endpoints',
-                destination: '/using-pgn/adding-pgn-to-a-wallet',
-                permanent: true
-            },
-            {
-                source: '/getting-started/bridge',
-                destination: '/using-pgn/bridging-eth-to-pgn',
-                permanent: true
-            },
-            {
-                source: '/getting-started/faucet',
-                destination: '/using-pgn/bridging-eth-to-pgn',
-                permanent: true
-            },
-            {
-                source: '/quick-links',
-                destination: '/',
-                permanent: true
-            }
-        ]
-    },
-}
-)
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.tsx',
+  })
+  
+  module.exports = withNextra(
+    {
+      i18n: {
+          locales: ['en'],
+          defaultLocale: 'en'
+        },    
+      async redirects() {
+          return [
+              {
+                  source: '/getting-started',
+                  destination: '/',
+                  permanent: true
+              },
+              {
+                  source: '/using-pgn',
+                  destination: '/using-pgn/adding-pgn-to-a-wallet',
+                  permanent: true
+              },
+              {
+                  source: '/getting-started/rpc-endpoints',
+                  destination: '/using-pgn/adding-pgn-to-a-wallet',
+                  permanent: true
+              },
+              {
+                  source: '/getting-started/bridge',
+                  destination: '/using-pgn/bridging',
+                  permanent: true
+              },
+              {
+                  source: '/getting-started/faucet',
+                  destination: '/using-pgn/bridging',
+                  permanent: true
+              },
+              {
+                  source: '/using-pgn/bridging-eth-to-pgn',
+                  destination: '/using-pgn/bridging',
+                  permanent: true
+              },
+              {
+                  source: '/quick-links',
+                  destination: '/',
+                  permanent: true
+              }
+          ]
+      },
+  }
+  )
